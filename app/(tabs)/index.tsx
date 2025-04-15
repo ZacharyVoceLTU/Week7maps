014aec5fc28e18b22d6e0b6effa5dbf8b19002bf
ZacharyVoceLTU <20731993@students.latrobe.edu.au>
2025-04-15 16:04:37 +1000

    more map

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,64 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView, {Circle, Marker, PROVIDER_DEFAULT} from 'react-native-maps';
 
 export default function App() {
+  const RADIUS = 200;
+
+  const markers = [
+    {coordinate: {
+      latitude: -37.721077,
+      longitude: 145.047977,
+    },
+    title: "Agora",
+    description: "My Coffee",
+    },
+    {
+      coordinate: {
+        latitude: -37.721407,
+        longitude: 145.046530,
+      },
+      title: "Beth Gleeson Building",
+      description: "My Home away from home",
+    },
+  ];
+
+  const renderMarkers = () => {
+    return markers.map((markers, index) => (
+      <Marker
+        key = {index}
+        coordinate = {markers.coordinate}
+        title = {markers.title}
+        description = {markers.description}
+      />
+    ));
+    };
+
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
-  );
+    <MapView
+      provider = {PROVIDER_DEFAULT}
+      style = {styles.map}
+      initialRegion = {{
+        latitude: -37.721,
+        longitude: 145.046,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+      >
+      {(renderMarkers())}
+
+      <Circle center = {{
+        latitude: -37.721407,
+        longitude: 145.046530,        
+      }} radius = {RADIUS}>
+      </Circle>
+
+      </MapView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   map: {
-    width: '100%',
-    height: '100%',
-  },
+    flex: 1,
+  }
 });
